0d33cdb2485e8e512be864ed1b30642458f5f48f
Uristamonov <Uristamonov@users.noreply.github.com>
2026-09-18 22:14:04 +0200

    Update countries.js

--- src/constants/countries.js
+++ src/constants/countries.js
@@ -1,1 +1,7 @@
-
+export const schengenCountries = [
+  "Austria", "Belgium", "Bulgaria", "Croatia", "Czech Republic", "Denmark", 
+  "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", 
+  "Italy", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", 
+  "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Slovakia", 
+  "Slovenia", "Spain", "Sweden", "Switzerland"
+];
