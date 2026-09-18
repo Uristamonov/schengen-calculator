28683bbdfbaf5904cd99ca71e2182cfaa0c61b3b
Uristamonov <Uristamonov@users.noreply.github.com>
2026-09-18 22:14:54 +0200

    Update dateHelpers.js

--- src/utils/dateHelpers.js
+++ src/utils/dateHelpers.js
@@ -1,1 +1,11 @@
+export const parseLocalDate = (str) => {
+  return !str ? new Date() : new Date(str + "T00:00:00");
+};
 
+export const formatDisplayDate = (str) => {
+  if (!str) return "Ongoing";
+  const dateObj = new Date(str + "T00:00:00");
+  if (isNaN(dateObj.getTime())) return str;
+  
+  return `${String(dateObj.getDate()).padStart(2, '0')}/${String(dateObj.getMonth() + 1).padStart(2, '0')}/${dateObj.getFullYear()}`;
+};
